bfd86c05a2a9ca3a6d74dbe0b4882be70eaa9b51
LuisGutWo <agutierrezwong@gmail.com>
2023-05-30 02:30:17 -0400

    fix responsive

--- src/layout/BackToTopButton.jsx
+++ src/layout/BackToTopButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const backToTopButton = () => {
   const [backToTopButton, setBackToTopButton] = useState(false);
@@ -18,20 +18,20 @@ const backToTopButton = () => {
   const scrollUp = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: "smooth",  
     });
   };
 
   return (
     <>
       {backToTopButton && (
-        <Button className="btn-arrow-up" onClick={scrollUp}>
+        <NavLink className="btn-arrow-up" onClick={scrollUp}>
           <img
             src="https://firebasestorage.googleapis.com/v0/b/login-huellitas.appspot.com/o/up-arrow-png-27167.png?alt=media&token=f1247d29-f328-4511-8a3d-582e688c839e"
             alt=""
             style={{ width: "3rem" }}
           />
-        </Button>
+        </NavLink>
       )}
     </>
   );
